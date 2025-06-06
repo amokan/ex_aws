1922a201e80a433d8ac4f99717507840c7332cc8
Adam Mokan <amokan@gmail.com>
2025-06-05 21:04:15 -0700

    adds endpoints for `s3tables` and `s3express`

--- priv/endpoints.exs
+++ priv/endpoints.exs
@@ -1943,6 +1943,53 @@ chime_voice_regions = [
           "isRegionalized" => true,
           "partitionEndpoint" => "us-east-1"
         },
+        "s3express" => %{
+          "defaults" => %{"protocols" => ["https"]},
+          "endpoints" => %{
+            "ap-northeast-1" => %{},
+            "ap-south-1" => %{},
+            "eu-north-1" => %{},
+            "eu-west-1" => %{},
+            "us-east-1" => %{},
+            "us-east-2" => %{},
+            "us-west-2" => %{}
+          }
+        },
+        "s3tables" => %{
+          "defaults" => %{"protocols" => ["https"]},
+          "endpoints" => %{
+            "af-south-1" => %{},
+            "ap-east-1" => %{},
+            "ap-northeast-1" => %{},
+            "ap-northeast-2" => %{},
+            "ap-northeast-3" => %{},
+            "ap-south-1" => %{},
+            "ap-south-2" => %{},
+            "ap-southeast-1" => %{},
+            "ap-southeast-2" => %{},
+            "ap-southeast-3" => %{},
+            "ap-southeast-4" => %{},
+            "ap-southeast-5" => %{},
+            "ca-central-1" => %{},
+            "ca-west-1" => %{},
+            "eu-central-1" => %{},
+            "eu-central-2" => %{},
+            "eu-north-1" => %{},
+            "eu-south-1" => %{},
+            "eu-south-2" => %{},
+            "eu-west-1" => %{},
+            "eu-west-2" => %{},
+            "eu-west-3" => %{},
+            "il-central-1" => %{},
+            "me-central-1" => %{},
+            "me-south-1" => %{},
+            "sa-east-1" => %{},
+            "us-east-1" => %{},
+            "us-east-2" => %{},
+            "us-west-1" => %{},
+            "us-west-2" => %{}
+          }
+        },
         "elasticbeanstalk" => %{
           "endpoints" => %{
             "af-south-1" => %{},
